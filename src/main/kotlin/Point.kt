81673ac3e0f490663659cce6519ccd11cd6444da
smengst <a02304220@usu.edu>
2025-06-30 15:38:49 -0600

    Point class

--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -1,3 +1,19 @@
-class Point (x: Double, y: Double){
+class Point(private var x: Double, private var y: Double) {
 
+    fun getX(): Double {
+        return x
+    }
+
+    fun getY(): Double {
+        return y
+    }
+
+    fun move(deltaX: Double, deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+
+    fun clone(): Point {
+        return Point(x, y);
+    }
 }
